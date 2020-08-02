b021156f50f385716ea5dcf7608ea4eff3a4cc30
Bernard Doci <bernarddoci@Bernards-MacBook-Pro.local>
2020-08-02 17:39:05 +0200

    Ready for deploy

---
+++ src/components/SearchBar.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const SearchBar = ({onFormSubmit}) => {
+    const [term, setTerm] = useState('');
+
+    const onSubmit = e => {
+        e.preventDefault();
+        onFormSubmit(term);
+    }
+
+    return (
+        <div className="search-bar ui segment">
+            <form className="ui form" onSubmit={onSubmit}>
+                <label>Search for a video</label>
+                <input type="text"
+                    value={term}
+                    onChange={e => setTerm(e.target.value)} />
+            </form>
+        </div>
+    )
+    
+}
+
+export default SearchBar;
